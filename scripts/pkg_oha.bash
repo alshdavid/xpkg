59fb0b537c94072d5870967baa055f7d613c8925
David Alsh <alshdavid@gmail.com>
2026-01-16 13:36:01 +1100

    pipelines

--- scripts/pkg_oha.bash
+++ scripts/pkg_oha.bash
@@ -20,17 +20,17 @@ fi
 
 echo "FETCHING"
 mkdir -p $TMP_DIR/binaries        
-mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-linux-amd64
-mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-linux-arm64
-mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-macos-amd64
-mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-macos-arm64
-mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-windows-amd64
+mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION-linux-amd64
+mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION-linux-arm64
+mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION-macos-amd64
+mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION-macos-arm64
+mkdir -p $TMP_DIR/binaries/$PACKAGE-$VERSION-windows-amd64
 
-wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-linux-amd64/oha        https://github.com/$REPO/releases/download/$VERSION/oha-linux-amd64
-wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-linux-arm64/oha        https://github.com/$REPO/releases/download/$VERSION/oha-linux-arm64
-wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-macos-amd64/oha        https://github.com/$REPO/releases/download/$VERSION/oha-macos-amd64
-wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-macos-arm64/oha        https://github.com/$REPO/releases/download/$VERSION/oha-macos-arm64
-wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION/$PACKAGE-$VERSION-windows-amd64/oha.exe  https://github.com/$REPO/releases/download/$VERSION/oha-windows-amd64.exe
+wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION-linux-amd64/oha        https://github.com/$REPO/releases/download/v$VERSION/oha-linux-amd64
+wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION-linux-arm64/oha        https://github.com/$REPO/releases/download/v$VERSION/oha-linux-arm64
+wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION-macos-amd64/oha        https://github.com/$REPO/releases/download/v$VERSION/oha-macos-amd64
+wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION-macos-arm64/oha        https://github.com/$REPO/releases/download/v$VERSION/oha-macos-arm64
+wget -q -O $TMP_DIR/binaries/$PACKAGE-$VERSION-windows-amd64/oha.exe  https://github.com/$REPO/releases/download/v$VERSION/oha-windows-amd64.exe
 
 echo "REPACKING"
 mkdir -p "$ROOT_DIR/binaries/$PACKAGE-$VERSION"
